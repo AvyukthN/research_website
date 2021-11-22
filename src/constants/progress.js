4071beb95487049f7564d24d7d2b4bfe08152a57
Avyukth Nilajagi <73862038+AvyukthN@users.noreply.github.com>
2021-11-22 11:51:23 -0500

    Update progress.js

--- src/constants/progress.js
+++ src/constants/progress.js
@@ -1,13 +1,13 @@
 export const research_meetings = [{
     title: 'Classical Implementation',
     date: '9/10/21',
-    summary: 'Implemented A* Search on a grid with automated wall generation for shortest pathfinding.',
+    summary: 'Implemented A* Search on a grid with automated wall generation for shortest pathfinding. Heuristic function can be easily modified so in the future, a quantum version can be easily substituted.',
     id: 0
 },
 {
     title: 'Markov Chain Implementation',
     date: '11/20/21',
-    summary: 'Implemented a general representation for the markov chain in order to use the qastar algorithm on an SSP problem.',
+    summary: 'Implemented a general representation for the markov chain in order to use the qastar algorithm on an SSP problem. This is an alternative for the previous graph implementation specific to the SSP problem. The A* function from the previous implementation can be implemented on the MarkovChain for SSP.',
     id: 1
 },
 {
