d53749281ffc72ca9cef0f88d75fb7c57c4ffde7
Avyukth Nilajagi <73862038+AvyukthN@users.noreply.github.com>
2022-02-04 09:01:12 -0500

    Update Progress.js

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -15,17 +15,30 @@ const Progress = () =>  (
     <GridContainer>
       {research_meetings.map((p, i) =>{
         return(
-          <Box>
-            <Paragraph>
-              <strong style={{fontSize: "20px;"}}>{p.title}</strong><br />
-              <Caption>
-                progress report - {p.date}
-              </Caption>
-              <br />
-              <br />
-              {p.summary}
-            </Paragraph>
-          </Box>
+          <div>
+            <Box>
+              <Paragraph>
+                <strong style={{fontSize: "20px;"}}>Replicating Grovers Search</strong><br />
+                <Caption>
+                  progress report - 2/2/22
+                </Caption>
+                <br />
+                <br />
+                <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSR1WF2vxoiUf3m5z0Og10gb2LLT3YXdcz-nSv5KFmqONvWIdWh2shjTw0fBgJ1ZXDsYUi65OaceKTk/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+              </Paragraph>
+            </Box>
+            <Box>
+              <Paragraph>
+                <strong style={{fontSize: "20px;"}}>{p.title}</strong><br />
+                <Caption>
+                  progress report - {p.date}
+                </Caption>
+                <br />
+                <br />
+                {p.summary}
+              </Paragraph>
+            </Box>
+          </div>
         );
       })}
     </GridContainer>
